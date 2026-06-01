9432a45867478f513586c15e400367ae4a9ab8b5
ibin0015 <ibin0015@student.monash.edu>
2026-06-01 20:32:51 +1000

    Html second change

--- vega_lite_vis.js
+++ vega_lite_vis.js
@@ -1,43 +1,34 @@
+var embedOpts = { actions: false, tooltip: false };
+
 var vg_1 = "Favourite_coffee.vg.json";
-vegaEmbed("#pie_favourite_coffee", vg_1, { actions: false, tooltip: false })
-  .catch(console.error);
+vegaEmbed("#pie_favourite_coffee", vg_1, embedOpts).catch(console.error);
 
 var vg_2 = "Coffee_consumption.vg.json";
-vegaEmbed("#map_world_consumption", vg_2, { actions: false, tooltip: false })
-  .catch(console.error);
+vegaEmbed("#map_world_consumption", vg_2, embedOpts).catch(console.error);
 
 var vg_3 = "Top_exporter.vg.json";
-vegaEmbed("#bar_top_exporters", vg_3, { actions: false, tooltip: false })
-  .catch(console.error);
+vegaEmbed("#bar_top_exporters", vg_3, embedOpts).catch(console.error);
 
 var vg_4 = "Coffee_bean_preference.vg.json";
-vegaEmbed("#lollipop_bean_preference", vg_4, { actions: false, tooltip: false })
-  .catch(console.error);
+vegaEmbed("#lollipop_bean_preference", vg_4, embedOpts).catch(console.error);
 
 var vg_5 = "aus_coffee_imports_flow.vg.json";
-vegaEmbed("#map_aus_imports_flow", vg_5, { actions: false, tooltip: false })
-  .catch(console.error);
+vegaEmbed("#map_aus_imports_flow", vg_5, embedOpts).catch(console.error);
 
 var vg_6 = "au_cafe_dotmap.vg.json";
-vegaEmbed("#map_aus_cafe_dots", vg_6, { actions: false, tooltip: false })
-  .catch(console.error);
+vegaEmbed("#map_aus_cafe_dots", vg_6, embedOpts).catch(console.error);
 
 var vg_7 = "Search_popularity.vg.json";
-vegaEmbed("#line_search_popularity", vg_7, { actions: false, tooltip: false })
-  .catch(console.error);
+vegaEmbed("#line_search_popularity", vg_7, embedOpts).catch(console.error);
 
 var vg_8 = "caffeine_bubble_chart.vg.json";
-vegaEmbed("#bubble_caffeine_by_age", vg_8, { actions: false, tooltip: false })
-  .catch(console.error);
+vegaEmbed("#bubble_caffeine_by_age", vg_8, embedOpts).catch(console.error);
 
 var vg_9 = "coffee_price_aud.vg.json";
-vegaEmbed("#bar_coffee_price_aud", vg_9, { actions: false, tooltip: false })
-  .catch(console.error);
+vegaEmbed("#bar_coffee_price_aud", vg_9, embedOpts).catch(console.error);
 
 var vg_10 = "flat_white_city_chart.vg.json";
-vegaEmbed("#strip_flatwhite_by_city", vg_10, { actions: false, tooltip: false })
-  .catch(console.error);
+vegaEmbed("#strip_flatwhite_by_city", vg_10, embedOpts).catch(console.error);
 
 var vg_11 = "flat_white_state_diverge.vg.json";
-vegaEmbed("#bar_flatwhite_state_diverge", vg_11, { actions: false, tooltip: false })
-  .catch(console.error);
+vegaEmbed("#bar_flatwhite_state_diverge", vg_11, embedOpts).catch(console.error);
